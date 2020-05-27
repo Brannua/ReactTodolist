e169f0cca380765a422c3e1c94795cba332c5927
Brannua <374453156@qq.com>
2020-05-27 14:15:32 +0800

    fix: doc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react';  // 只有引入'react'包才可以编译JSX语法
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react';  // 只有引入'react'包才可以编译JSX语法
 import ReactDOM from 'react-dom';
 import App from './App';
 

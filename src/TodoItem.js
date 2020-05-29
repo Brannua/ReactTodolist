4a240aafa3cf94574b584abc38d2b779318b9135
Brannua <374453156@qq.com>
2020-05-29 12:44:28 +0800

    feature: propTypes typechecking & define default values for props

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -12,13 +12,27 @@
 
 // 函数式改写
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function TodoItem(props) {
   return (
     <li
       onClick={ () => { props.handleDeleteItem(props.idx) } }
-    >{ props.content }</li>
+    >{ props.defaultProp }--{ props.content }</li>
   )
-} 
+}
+
+// propTypes typechecking
+TodoItem.propTypes = {
+  handleDeleteItem: PropTypes.func,
+  idx: PropTypes.number,
+  content: PropTypes.string,
+  defaultProp: PropTypes.string.isRequired,
+};
+
+// define default values for props
+TodoItem.defaultProps = {
+  defaultProp: '默认prop'
+}
 
 export default TodoItem;

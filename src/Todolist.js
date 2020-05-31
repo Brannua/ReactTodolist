389ff65ed31e648287da13285bba9ec458b4db14
Brannua <374453156@qq.com>
2020-05-31 18:08:16 +0800

    feature: 创建reducer & store

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,25 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-const data = [
-  'sdfghjk',
-  'sdfghjk',
-  'sdfghjk',
-  'sdfghjk',
-];
+import store from './store';
 
 class Todolist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+  }
   render() {
     return (
       <Fragment>
         <div>
-          <Input placeholder='todo info' style={{ width: '300px', margin: '10px' }}></Input>
+          <Input placeholder={ this.state.inputVal } style={{ width: '300px', margin: '10px' }}></Input>
           <Button type='primary'>提交</Button>
         </div>
         <List
           bordered
-          dataSource={ data }
+          dataSource={ this.state.list }
           renderItem={ item => ( <List.Item>{ item }</List.Item> ) }
           style={{ width: '380px', marginLeft: '10px' }}
         ></List>
---
+++ src/store/reducer.js
@@ -0,0 +1,15 @@
+/**
+ * @desc reducer
+ */
+
+// 默认 state
+const defaultState = {
+  inputVal: 'please input sth...',
+  list: [1, 2, 3],
+};
+
+function reducer(prevState = defaultState, action) {
+  return prevState;
+}
+
+export default reducer;

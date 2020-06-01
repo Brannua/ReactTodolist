372fbbcdaa180e32f64fb5c2836469b750fb4e06
Brannua <374453156@qq.com>
2020-06-01 17:06:43 +0800

    feature: 参考redux-devtools文档，使用其中描述的enhencer增强器写法，将redux-devtools扩展程序和redux-thunk中间件集成在一起生成增强器，供redux使用

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,27 @@
 /**
  * @desc store
+ * https://github.com/zalmoxisus/redux-devtools-extension
  */
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './reducer';
 
+// 生成构造增强器的函数
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+// 生成增强器
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+);
+
+// 使用(reducer & 增强器)来初始化创建store
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  enhancer
 );
 
 export default store;
